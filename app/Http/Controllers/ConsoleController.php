911682ecc2f7b1ea5218f1ce40334ac57ccc4762
YujiaWang6 <wang.yujia111@gmail.com>
2023-10-07 01:05:07 -0400

    modified consolecontroller

--- app/Http/Controllers/ConsoleController.php
+++ app/Http/Controllers/ConsoleController.php
@@ -45,36 +45,3 @@ class ConsoleController extends Controller
         return redirect('/');
     }
 }
-
-
-   /* public function dashboard(){
-        return view('console.dashboard');
-    }
-
-    public function loginForm(){
-        return view('console.login');
-    }
-
-    public function login(){
-        $attributes = request()->validate([
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
-
-        if(auth()->attempt($attributes))
-        {
-            return redirect('/console/dashboard');
-        }
-
-        return back()
-            ->withInput()
-            ->withErrors(['email' => 'Invalid email/password combination']);
-    }
-
-    public function logout(){
-        auth()->logout();
-        return redirect('/');
-    }
-
-
-}*/
