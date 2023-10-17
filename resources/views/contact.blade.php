cf35aa241c8f85a318b11c6eea6f5d6bca063aea
YujiaWang6 <wang.yujia111@gmail.com>
2023-10-17 02:56:43 -0400

    added styling for home page

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+         <!-- Styles -->
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="/app.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <title>Dog Park Finder | Contact</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <style>
+            .search-box{
+                background-color: rgb(255, 255, 255, 0.75); width:65vw; height:34vh; display: flex; flex-direction: column; justify-content: center; align-items: center; border-radius:15px;
+            }
+            @media screen and (max-width:1000px){
+                .search-box{
+                    width:90vw;
+                    height:34vh;
+                }
+            }
+        </style>
+
+    </head>
+
+    <body>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                    <a class="navbar-brand" href="/">Dog Park Finder</a>
+                    <ul class="navbar-nav me-auto mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="/">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/about">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="/contact">Contact</a>
+                        </li>
+                    </ul>
+                    
+                    <div class="mb-2 mb-lg-0 navbar-nav">
+                        <?php if(Auth::check() && auth()->user()->user_role ==='admin'):?>
+                            <div class="nav-item">
+                                <a href='/console/dashboard' class="nav-link">Dashboard</a>
+                            </div>
+                            <div class="nav-item">
+                                <a href='/console/logout' class="nav-link">log out</a>
+                            </div>
+                        <?php elseif(Auth::check() && auth()->user()->user_role === 'user'): ?>
+                            <a href='/console/users/profile/<?= auth()->user()->id?>' class="nav-link">Profile</a>
+                            <a href='/console/logout' class="nav-link">Log out</a>
+                        <?php else:?>
+                            <div class="nav-item">
+                                <a href='/console/login' class="nav-link">Login</a>
+                            </div>
+                            <div class="nav-item">
+                                <a href='/console/users/add' class="nav-link">Create an account</a>
+                            </div>
+                        <?php endif;?>
+                    </div>
+                    
+                </div>
+            </div>
+        </nav>
+
+
+
+        <footer class="container-fluid">
+            <p>©Copy right Dog Park Finder, 2023</p>
+        </footer>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    </body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,32 +7,92 @@
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <link rel="stylesheet" href="/app.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
         <title>Dog Park Finder | Home</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <style>
+            .search-box{
+                background-color: rgb(255, 255, 255, 0.75); width:65vw; height:34vh; display: flex; flex-direction: column; justify-content: center; align-items: center; border-radius:15px;
+            }
+            @media screen and (max-width:1000px){
+                .search-box{
+                    width:90vw;
+                    height:34vh;
+                }
+            }
+        </style>
+
     </head>
 
     <body>
-        <div>
-            <?php if(Auth::check() && auth()->user()->user_role ==='admin'):?>
-                <a href='/console/dashboard'>Dashboard</a>
-                <a href='/console/logout'>log out</a>
-            <?php elseif(Auth::check() && auth()->user()->user_role === 'user'): ?>
-                <a href='/console/users/profile/<?= auth()->user()->id?>'>Profile</a>
-                <a href='/console/logout'>Log out</a>
-            <?php else:?>
-                <a href='/console/login'>Login</a>
-                <a href='/console/users/add'>Create an account</a>
-            <?php endif;?>
-        </div>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                    <a class="navbar-brand" href="/">Dog Park Finder</a>
+                    <ul class="navbar-nav me-auto mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="/">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/about">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/contact">Contact</a>
+                        </li>
+                    </ul>
+                    
+                    <div class="mb-2 mb-lg-0 navbar-nav">
+                        <?php if(Auth::check() && auth()->user()->user_role ==='admin'):?>
+                            <div class="nav-item">
+                                <a href='/console/dashboard' class="nav-link">Dashboard</a>
+                            </div>
+                            <div class="nav-item">
+                                <a href='/console/logout' class="nav-link">log out</a>
+                            </div>
+                        <?php elseif(Auth::check() && auth()->user()->user_role === 'user'): ?>
+                            <a href='/console/users/profile/<?= auth()->user()->id?>' class="nav-link">Profile</a>
+                            <a href='/console/logout' class="nav-link">Log out</a>
+                        <?php else:?>
+                            <div class="nav-item">
+                                <a href='/console/login' class="nav-link">Login</a>
+                            </div>
+                            <div class="nav-item">
+                                <a href='/console/users/add' class="nav-link">Create an account</a>
+                            </div>
+                        <?php endif;?>
+                    </div>
+                    
+                </div>
+            </div>
+        </nav>
 
-        <section>
-            <form action="/parksresult" method="get">
-                <input type="text" name="location" id="location">
-                <button type="submit" id="submitBtn">Search</button>
-            </form>
+        <section class="d-flex" style="background-image: url({{ asset('background.jpeg') }}); background-size:cover; background-repeat: no-repeat; background-position: center center; height: 90vh;">
+            <div class="container-sm d-flex justify-content-center align-self-center">
+                <div class="text-center search-box">
+                    <div>   
+                        <h1 class="h1">Search the dog parks in your region</h1>
+                        <form action="/parksresult" method="get" class="input-group">
+                            <input type="text" name="location" id="location" placeholder="e.g. M6G 1L1 or M6G" class="form-control rounded">
+                            <button type="submit" id="submitBtn" class="btn btn-primary">Search</button>
+                        </form>
+                    </div> 
+                </div>
+            </div>
         </section>
+
+        <footer class="container-fluid">
+            <p>©Copy right Dog Park Finder, 2023</p>
+        </footer>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
     </body>
 </html>
